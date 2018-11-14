249ab52b9c09c3d7222db0d32eeaaf049f0ff928
Beno!t POLASZEK <bpolaszek@gmail.com>
2018-11-14 22:52:25 +0100

    Registry is completely useless here

--- src/Twig/WebPushTwigExtension.php
+++ src/Twig/WebPushTwigExtension.php
@@ -2,28 +2,19 @@
 
 namespace BenTools\WebPushBundle\Twig;
 
-use BenTools\WebPushBundle\Model\Subscription\UserSubscriptionManagerRegistry;
 use Twig\Extension\AbstractExtension;
-use Twig_Extension_GlobalsInterface;
+use Twig\Extension\GlobalsInterface;
 
-final class WebPushTwigExtension extends AbstractExtension implements Twig_Extension_GlobalsInterface
+final class WebPushTwigExtension extends AbstractExtension implements GlobalsInterface
 {
     /**
      * @var string
      */
     private $publicKey;
 
-    /**
-     * @var UserSubscriptionManagerRegistry
-     */
-    private $registry;
-
-    public function __construct(
-        string $publicKey,
-        ?UserSubscriptionManagerRegistry $registry = null
-    ) {
+    public function __construct(string $publicKey)
+    {
         $this->publicKey = $publicKey;
-        $this->registry = $registry;
     }
 
     /**
